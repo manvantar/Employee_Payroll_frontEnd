99f4830106b585e3e8f21849e5791002d901f1b3
manvantar <manukvshetty@gmail.com>
2021-07-10 10:49:16 +0530

    feat : adding Navbar/header in dashboard

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import Signup  from './pages/register'
 import Login from './pages/login'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import ClippedDrawer from './components/dashboard';
-//import history from './history';
-
+import SideBar from './components/dashboard2/Sidebar';
+import Navbar from './components/dashboard/navbar'
 const App = () => {
   return (
     <Router >
@@ -13,7 +13,7 @@ const App = () => {
           <Route exact path='/' component={Signup} />
             <Route path='/register' component={Signup} />
             <Route path='/login' component={Login} />
-            <Route path='/dashboard' component={ClippedDrawer} />
+            <Route path='/dashboard' component={Navbar} />
           </Switch>
       </div>
     </Router>
---
+++ src/components/dashboard/Menuitems.js
@@ -0,0 +1,17 @@
+export const MenuItems=[
+    {
+        title: 'support',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'tools',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'signup',
+        url: '#',
+        cName: 'nav-links'
+    }
+]
