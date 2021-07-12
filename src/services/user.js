197fdaee85a36ffc2f597c03ac3feeb5357361fd
manvantar <manukvshetty@gmail.com>
2021-07-12 11:57:49 +0530

    feat: adding comments to User services.js

--- src/services/user.js
+++ src/services/user.js
@@ -3,10 +3,20 @@ axios.defaults.baseURL = process.env.React_App_BASEURL;
 
 class User {
 
+    /**
+    * @description integrating FrontEnd request to Backend User Login API using axious
+    * @param credentials i.e emailId and password
+    * @return response from the backend to Login page
+    */
     login = (credentials) => {
         return axios.post("/login", credentials)
     };
 
+    /**
+    * @description integrating FrontEnd request to Backend User Registration API using axious
+    * @param credentials i.e firstName, lastName, emailId and password
+    * @return response from the backend to Registartion page
+    */
     SignUpData = (userData) => {
         return axios.post("/registration", userData)
     };
