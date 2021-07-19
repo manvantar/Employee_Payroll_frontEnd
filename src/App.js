e25aa73091d8b080df5bdfbf45533694351913bf
manvantar <manukvshetty@gmail.com>
2021-07-19 21:05:43 +0530

    refactor: remove unused files

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Signup from "./pages/register";
 import Login from "./pages/login";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import dashboard from "./components/dashboard";
-import ProtectedRouter from "./services/protectedRouter.js";
+
 /**
  * @description App functional component to return multiple componets on routing
  * @return router with multiple components on routing
