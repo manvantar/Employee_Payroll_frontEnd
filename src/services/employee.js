2d7c7f1d17adfe3f6ceda6f13aa865252e50e76d
manvantar <manukvshetty@gmail.com>
2021-07-19 17:00:05 +0530

    feat: adding axios update employee method

--- src/services/employee.js
+++ src/services/employee.js
@@ -2,13 +2,15 @@ import axios from "axios";
 axios.defaults.baseURL = process.env.React_App_BASEURL;
 
 class Employee {
-  
+
   /**
-   * @description integrating FrontEnd request to Backend Employee Lists API using axious
-   * @return response from the backend to Dashboard having Employee details
+   * @description Method to return token from local storage
+   * @return token within headers
    */
-  getToken =  () => {
-    const headers=  { headers: { Authorization: localStorage.getItem("token") } };
+  getToken = () => {
+    const headers = {
+      headers: { Authorization: localStorage.getItem("token") },
+    };
     return headers;
   };
 
@@ -47,15 +49,27 @@ class Employee {
   deleteEmployee = async (employeeId) => {
     const headers = this.getToken();
     try {
-      const res = await axios.delete(
-        "/delete/employee/" + employeeId,
-        headers
-      );
+      const res = await axios.delete("/delete/employee/" + employeeId, headers);
       return res;
     } catch (error) {
       return error;
     }
   };
+
+  /**
+   * @description integrating FrontEnd request to Backend to update an Employee Data using axious
+   * @return response from the backend to Dashboard having success or failure details for update
+   */
+   updateEmployee = async (employeeData, employeeId) => {
+    const headers = this.getToken();
+    try {
+      const res = await axios.put("/update/employee/" + employeeId, employeeData, headers);
+      return res;
+    } catch (error) {
+      return error;
+    }
+  };
+
 }
 
 export default new Employee();
